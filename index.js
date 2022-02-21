2afddfd40df0b445a50af4869d376afd3661857c
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-02-20 22:58:48 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+var num1 = 31
+var num2 = 2
+var multiply= num1 * num2;
+var random=Math.floor(Math.random() * 10)+1;
+var num3 = 14
+var num4 = 5
+var mod= num3 % num4;
+var max= Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
